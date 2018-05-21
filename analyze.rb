6284772aef49290d120c56f9a792e9186b5b8c72
Caleb Smith <karagenit@outlook.com>
2018-05-21 09:59:13 -0400

    Track Response Word Counts

--- analyze.rb
+++ analyze.rb
@@ -4,6 +4,7 @@ require 'csv'
 
 data = CSV.parse(IO.read('data.csv'), headers: true)
 
+# TODO: just print all of them
 puts "Headers: "
 data.headers.each_with_index do |header, index|
   puts "#{index}. #{header}"
@@ -13,20 +14,24 @@ print "Column to Analyze: "
 colIdx = gets.chomp.to_i
 puts "-----"
 
-totals = {}
+responses = {} # Word Frequency Maps
+counts = [] # Correlations between # of words responded and values
 
 data.each do |row|
+  counts.push [row[1]&.split(/[\s,\/]/)&.length.to_i, row[9] || "None"]
   row.each do |col, value|
-    totals[col] = {} if totals[col].nil?
+    responses[col] = {} if responses[col].nil?
     value ||= '' # prevent nil NoMethod error
     value.split(/[\s,\/]/).each do |value|
       value = '[blank]' if value.empty?
       value = value.strip.downcase
-      totals[col][value] = totals[col][value].to_i + 1
+      responses[col][value] = responses[col][value].to_i + 1
     end
   end
 end
 
-totals[totals.keys[colIdx]].sort_by{ |response, count| -count }.first(20).each do |resp, count|
+responses[responses.keys[colIdx]].sort_by{ |response, count| -count }.first(20).each do |resp, count|
   puts "#{resp} (#{count})"
 end
+
+File.write("counts.csv", counts.map(&:to_csv).join)
